e8d45b37aa6e30cc6da3f1d264816211491d2367
Janez Podlogar <janez.podlogar@outlook.com>
2022-04-29 23:08:16 +0200

    Dva razlicna formata podatkov CB

--- lib/funkcije.r
+++ lib/funkcije.r
@@ -1,7 +1,6 @@
 library(aws.s3)
-
+# Sprejeme leto v formatu YYYY in je vkljucno od in do vnesenega leta
 PrenosCB <- function(OdLeto = 2010, DoLeto) {
-  # Sprejeme leto v formatu YYYY in je vkljucno od in do vnesenega leta
   bucket <- get_bucket('capitalbikeshare-data')
   bucket <- bucket[-length(bucket)]
   seznam <- vector('character', length(bucket))
@@ -9,7 +8,10 @@ PrenosCB <- function(OdLeto = 2010, DoLeto) {
     DoLeto <- OdLeto
   }
   for (i in bucket) {
-    leto <- i$Key %>% substr(1,4) %>% strtoi()
+    leto <- i$Key %>% 
+      substr(1,4) %>% 
+      strtoi()
+    
     if (leto >= OdLeto & leto <= DoLeto) {
       temp <- tempfile()
       save_object(i$Key, file = temp, bucket = "capitalbikeshare-data")
@@ -19,3 +21,17 @@ PrenosCB <- function(OdLeto = 2010, DoLeto) {
   }
 }
 
+# Prebere imena stolpcev v seznam v odvisnosti od vzorca
+ImenaStolpcev <- function(pattern) {
+  ImeDatotekCB <- list.files('./podatki', pattern = pattern, full.names = TRUE)
+  ImeStolpcevCB <- vector(mode = 'list', length = length(ImeDatotekCB))
+  k <- 1
+  for (i in ImeDatotekCB) {
+    stolpci <- read_csv(i, col_names = TRUE, n_max = 0, show_col_types = FALSE) %>%
+      colnames()
+    ImeStolpcevCB
+    ImeStolpcevCB[[k]] <- stolpci
+    k <- k +1
+  }
+  return(ImeStolpcevCB)
+}
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,5 +1,7 @@
 # 2. faza: Uvoz podatkov
 
+# sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+
 #  Podatki o zgodovini vozenj s kolesi se zacenjo leta 2010. Do vkljucno leta
 # 2017 so bili podatki na AWS nalozeni letno, poznejsi pa so nalozeni mesecno.
 # Ker podatki do vklucno leta 2017 vsebujejo cetrtletne datoteke se bo izvedel
@@ -8,28 +10,37 @@
 
 source('lib/funkcije.r', encoding='UTF-8')
 
-PrenosCB(2011,2021)
+PrenosCB(2011, 2022)
+
+ImenaStolpcevCB <- ImenaStolpcev('tripdata')
+
+#  Ko na hitro pregledamo imena stolpcev tabel, se nam zdi, da so stolpci v
+# tabelah enako poimenovane od tabele 1 (2011) do vkljucno tabele 52 (202003).
+# Stolpci vsebujejo cas voznje, datum zacetka in konca voznje, ime in stevilka
+# zacetne in koncne postaje, stevilko kolesa ter tip narocnine.
+#
+#  Sledi spremeba v poimenovanju stolpcev, ki je dosledna za vse sledece
+# podatke. Dodani se tudi stolpci za ID voznje, tip kolesa ter geografsko sirino in
+# dolzino zacetne in koncne postaje. Odstranjen je stolpec za cas voznje.
+#
+#  S spodnjo zanko lahko preverimo, da so stolpci res dosledni s tabelo 1 do
+# tabele 52 ter da so dosledni s tabelo 53 do zadnje tabele 73.
+#  
+# for (i in 1:length(ImenaStolpcevCB)) {
+#   if (i <= 52) {
+#     if (all(ImenaStolpcevCB[[i]] == ImenaStolpcevCB[[52]])) {
+#       ImenaStolpcevCB[[i]] = TRUE
+#     }
+#   }
+#   else {
+#     if (all(ImenaStolpcevCB[[i]] == ImenaStolpcevCB[[length(ImenaStolpcevCB)]])) {
+#       ImenaStolpcevCB[[i]] = TRUE
+#     }
+#   }
+# }
+# all(ImenaStolpcevCB) == TRUE
+# ImenaStolpcevCB <- ImenaStolpcev('tripdata')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
