0ade9793df2add407f5ce3c79d2816ddf538d1d2
Janez Podlogar <janez.podlogar@outlook.com>
2022-08-25 22:03:57 +0200

    Prečno preverjanje

--- lib/funkcije.r
+++ lib/funkcije.r
@@ -52,3 +52,138 @@ drseca_vsota = function(x, n = 14) {
   }
   return(cx)
 }
+
+# Učenje modelov
+ucenje = function(podatki, formula, algoritem) {
+  switch(
+    algoritem,
+    lin.reg = lm(formula, data = podatki),
+    ng= ranger(formula, data = podatki)
+  )
+}
+
+napovedi = function(podatki, model, algoritem) {
+  switch(
+    algoritem,
+    lin.reg = predict(model, podatki),
+    log.reg = ifelse(
+      predict(model, podatki, type = "response") >= 0.5,
+      1, -1
+    ),
+    ng = predict(model, podatki)$predictions
+  )
+}
+
+napaka_regresije = function(podatki, model, algoritem) {
+  podatki %>%
+    bind_cols(yn.hat = napovedi(podatki, model, algoritem)) %>%
+    mutate(
+      izguba = (yn - yn.hat) ^ 2
+    ) %>%
+    select(izguba) %>%
+    unlist() %>%
+    mean()
+}
+
+napaka_razvrscanja = function(podatki, model, algoritem) {
+  podatki %>%
+    bind_cols(yd.hat = napovedi(podatki, model, algoritem)) %>%
+    mutate(
+      izguba = (yd != yd.hat)
+    ) %>%
+    select(izguba) %>%
+    unlist() %>%
+    mean()
+}
+
+# Razrez vektor x na k enako velikih delov
+razbitje = function(x, k) {
+  
+  # Razreži indekse vektorja na k intervalov
+  razrez = cut(seq_along(x), k, labels = FALSE)
+  
+  # Razbij vektor na k seznamov
+  # na osnovi razreza intervalov
+  split(x, razrez)
+}
+
+pp.razbitje = function(n, k = 10, stratifikacija = NULL, seme = NULL) {
+  
+  # najprej nastavimo seme za naključna števila, če je podano
+  if (!is.null(seme)) {
+    set.seed(seme)
+  }
+  
+  # če ne opravljamo stratifikacije, potem vrnemo navadno razbitje
+  # funkcijo sample uporabimo zato, da naključno premešamo primere
+    return(razbitje(sample(1:n), k))
+}
+
+# Doda diskretno spremenljivka nd, ki ima vrednost 1, za n > mediana in -1 sicer.
+indikator.nd = function(tbl) {
+  tbl %>% 
+    mutate(
+      nd = if_else( n > median(tbl$n), 1, -1),
+      nd = factor(nd, levels = c(-1,1))
+    ) 
+}
+
+pp.razbitje = function(n, k = 10, stratifikacija = NULL, seme = NULL) {
+  
+  # najprej nastavimo seme za naključna števila, če je podano
+  if (!is.null(seme)) {
+    set.seed(seme)
+  }
+  
+  # če ne opravljamo stratifikacije, potem vrnemo navadno razbitje
+  # funkcijo sample uporabimo zato, da naključno premešamo primere
+  if (is.null(stratifikacija)) {
+    return(razbitje(sample(1:n), k))
+  }
+  
+  # če pa opravljamo stratifikacijo, razbitje izvedemo za vsako
+  # vrednost spremenljive stratifikacija posebej in nato
+  # podmnožice združimo v skupno razbitje
+  r = NULL
+  for (v in levels(stratifikacija)) {
+    
+    # Če smo pri prvi vrednosti vzpostavimo razbitje
+    if (is.null(r)) {
+      # opravimo razbitje samo za primere z vrednostjo v
+      r = razbitje(sample(which(stratifikacija == v)), k)
+    } else {
+      # opravimo razbitje za vrednost v
+      # in podmnožice združimo s trenutnim razbitjem
+      r.v = razbitje(sample(which(stratifikacija == v)), k)
+      for (i in 1:k) {
+        r[[i]] = c(r[[i]], r.v[[i]])
+      }
+    }
+  }
+  r
+}
+
+precno.preverjanje = function(podatki, razbitje, formula, algoritem, razvrscanje) {
+  
+  # pripravimo vektor za napovedi
+  if (razvrscanje) {
+    pp.napovedi = factor(rep(1, nrow(podatki)), levels = c(-1,1))
+  } else {
+    pp.napovedi = rep(0, nrow(podatki))
+  }
+  
+  # gremo čez vse podmnožice Si razbitja S
+  for (i in 1:length(razbitje)) {
+    # naučimo se modela na množici S \ Si
+    model = podatki[ -razbitje[[i]], ] %>% ucenje(formula, algoritem)
+    
+    # naučen model uporabimo za napovedi na Si
+    pp.napovedi[ razbitje[[i]] ] = podatki[ razbitje[[i]], ] %>% napovedi(model, algoritem)
+  }
+  
+  if (razvrscanje) {
+    mean(pp.napovedi != podatki$n)
+  } else {
+    mean((pp.napovedi - podatki$n) ^ 2)
+  }
+}
